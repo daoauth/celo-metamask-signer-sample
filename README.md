ceabb04c6d4fb920bb8475e34432f8e6f286cb7c
daoauth <57783762+daoauth@users.noreply.github.com>
2020-09-08 16:29:25 +0900

    sample code

--- README.md
+++ README.md
@@ -1,5 +1,37 @@
 # celo-metamask-signer-sample
 
+## Sample Code
+[index.vue](https://github.com/daoauth/celo-metamask-signer-sample/blob/master/pages/index.vue)
+```javascript
+import { sendTransaction } from '@did-kr-cg/celo-metamask-signer'
+const ContractKit = require('@celo/contractkit')
+const Web3 = require('web3')
+
+...
+
+async function sendTx () {
+  const kit = ContractKit.newKit('https://alfajores-forno.celo-testnet.org')
+  let web3 = null
+
+  if (typeof window.ethereum !== 'undefined') {
+    await window.ethereum.enable()
+    web3 = new Web3(window.ethereum)
+  } else {
+    return
+  }
+  const accounts = await this.web3.eth.getAccounts()
+
+  const rawTx = {
+    from: accounts[0],
+    to: 'TARGET ACCOUNT',
+    value: 'VALUE',
+    data: 'IF YOU WANT USE SMART CONTRACT'
+  }
+  const receipt = await sendTransaction(kit, web3, rawTx)
+  console.log(`https://alfajores-blockscout.celo-testnet.org/tx/${receipt.transactionHash}`)
+}
+```
+
 ## Build Setup
 
 ```bash
--- pages/index.vue
+++ pages/index.vue
@@ -1,16 +1,172 @@
 <template>
-  <div class="container">
-    <div>
-      <Logo />
-      <h1 class="title">
-        celo-metamask-signer-sample
-      </h1>
+  <div class="container is-fluid">
+    <div class="card">
+      <header class="card-header">
+        <p class="card-header-title">
+          <span class="icon">
+            <img src="/celo-metamask-signer-sample/icon-celo-CELO-color-f.svg">
+          </span>
+          &nbsp;
+          MetaCelo - MetaMask Celo TX Signer
+        </p>
+      </header>
+      <div class="card-content">
+        <div class="control">
+          <div class="select">
+            <select>
+              <option selected>https://alfajores-forno.celo-testnet.org</option>
+            </select>
+          </div>
+        </div>
+        <br>
+        <div class="field">
+          <p class="control has-icons-left">
+            <input v-model="account" class="input" type="text" readonly>
+            <span class="icon is-small is-left">
+              <i class="fas fa-wallet" />
+            </span>
+          </p>
+        </div>
+        <div v-for="item in balance" :key="item.name" class="field">
+          <p class="control has-icons-left">
+            <input v-model="item.value" class="input" type="text" readonly>
+            <span class="icon is-small is-left">
+              <img :src="item.icon">
+            </span>
+          </p>
+        </div>
+        <a
+          :href="linkAddress"
+          target="_blank"
+          rel="noopener noreferrer"
+          class="button--grey"
+        >
+          Blockscout - Address
+        </a>
+        <hr class="dashed">
+        <div class="field is-horizontal">
+          <div class="field-body">
+            <div class="field is-expanded">
+              <div class="field has-addons">
+                <p class="control">
+                  <a class="button is-static">
+                    To
+                  </a>
+                </p>
+                <p class="control is-expanded">
+                  <input v-model="to" class="input" type="text">
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="field is-horizontal">
+          <div class="field-body">
+            <div class="field is-expanded">
+              <div class="field has-addons">
+                <p class="control">
+                  <a class="button is-static">
+                    Value
+                  </a>
+                </p>
+                <p class="control is-expanded">
+                  <input v-model="value" class="input" type="number">
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="links">
+          <a
+            v-if="!sended"
+            class="button--grey"
+            @click="send"
+          >
+            Send
+          </a>
+          <a
+            v-if="sended && linkTx"
+            :href="linkTx"
+            target="_blank"
+            rel="noopener noreferrer"
+            class="button--grey"
+          >
+            Blockscout - Transaction
+          </a>
+        </div>
+      </div>
     </div>
   </div>
 </template>
 
 <script>
-export default {}
+import { sendTransaction } from '@did-kr-cg/celo-metamask-signer'
+const ContractKit = require('@celo/contractkit')
+const Web3 = require('web3')
+
+export default {
+  data () {
+    return {
+      account: 'loading...',
+      web3: null,
+      kit: null,
+      balance: [
+        { name: 'CELO', icon: '/celo-metamask-signer-sample/icon-celo-CELO-dark-f.svg', value: 'loading...' },
+        { name: 'cUSD', icon: '/celo-metamask-signer-sample/icon-celo-dollar-black-f.svg', value: 'loading...' }
+      ],
+      to: '',
+      value: '',
+      sended: false,
+      linkAddress: null,
+      linkTx: null
+    }
+  },
+
+  async mounted () {
+    if (typeof window.ethereum !== 'undefined') {
+      await window.ethereum.enable()
+      this.web3 = new Web3(window.ethereum)
+    } else {
+      return
+    }
+    const accounts = await this.web3.eth.getAccounts()
+    this.account = accounts[0]
+
+    this.kit = ContractKit.newKit('https://alfajores-forno.celo-testnet.org')
+    const totalBalance = await this.kit.getTotalBalance(this.account)
+    for (let i = 0; i < this.balance.length; i++) {
+      this.balance[i].value = this.kit.web3.utils.fromWei(totalBalance[this.balance[i].name].toString())
+    }
+    // this.balance.push({ name: 'Locked', value: this.kit.web3.utils.fromWei(totalBalance.lockedCELO.toString()) })
+    // this.balance.push({ name: 'Panding', value: this.kit.web3.utils.fromWei(totalBalance.pending.toString()) })
+
+    this.linkAddress = `https://alfajores-blockscout.celo-testnet.org/address/${this.account}`
+  },
+
+  methods: {
+    async send () {
+      const transactionParameters = {
+        from: this.account,
+        to: this.to,
+        value: this.value
+      }
+      this.sended = true
+      const receipt = await sendTransaction(this.kit, this.web3, transactionParameters)
+      this.linkTx = `https://alfajores-blockscout.celo-testnet.org/tx/${receipt.transactionHash}`
+    }
+  }
+
+  /*
+  async mounted () {
+    const transactionParameters = {
+      from: '',
+      to: '',
+      value: '1'
+    }
+    await web3(transactionParameters)
+  }
+  */
+}
 </script>
 
 <style>
